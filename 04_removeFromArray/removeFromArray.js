1781b63e1daa24854199671c16abd85bf913bcba
mdcergo <182567932+mdcergo@users.noreply.github.com>
2025-04-23 10:26:30 -0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
 const removeFromArray = function() {
+    let args = Array.from(arguments);
+    argumentNum = args.length
+    let array = args.shift()
+    for (remove of args) {
+        let index = array.indexOf(remove)
+        while (index !== -1) {
+            array.splice(index, 1)
+            index = array.indexOf(remove)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
