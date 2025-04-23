8634039f8af484f35af8d040ff8b5a91ad659b30
mdcergo <182567932+mdcergo@users.noreply.github.com>
2025-04-23 11:15:04 -0500

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celcius = ((fahrenheit - 32) * (5/9))
+  return temperature = parseFloat(celcius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  fahrenheit = ((celcius * (9/5)) + 32)
+  return temperature = parseFloat(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
