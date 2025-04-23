dd673ffb4810b152fa3cdac2f863b1903d0c3600
mdcergo <182567932+mdcergo@users.noreply.github.com>
2025-04-23 09:40:20 -0500

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let length = string.length
+    newString = ""
+    for (i = 1; i <= length; i++) {
+        newString += string.charAt(length - i)
+        console.log(newString)
+    }
+    console.log(newString)
+    return newString
 };
 
 // Do not edit below this line
