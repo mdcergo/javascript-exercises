564bdd9a0aae65e4d946d0ea06b51371fd1ee2c6
mdcergo <182567932+mdcergo@users.noreply.github.com>
2025-04-23 09:34:26 -0500

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR"
+    let newString = ""
+    for (let i = 1; i <= num; i++) {
+        newString = (newString + string)
+    }
+    string = newString
+    return string
 };
 
 // Do not edit below this line
